de6c23fb6aa904b78b30368444f0c970f4cbf782
Nathan Bowdish <djnatronic@gmail.com>
2020-08-15 08:43:06 -0700

    update mongo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 443;
+const PORT = 80;
 
 const app = express();
 
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nathanbprogressivebudget');
 
-/* mongoose.connect("mongodb://localhost/nathanbprogressivebudget", {
+
+ mongoose.connect("mongodb://localhost/nathanbprogressivebudget", {
   useNewUrlParser: true,
   useFindAndModify: false
-}); */
+}); 
 
 // routes
 app.use(require("./routes/api.js"));
